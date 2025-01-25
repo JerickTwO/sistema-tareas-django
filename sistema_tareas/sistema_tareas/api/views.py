15b7675458bdb7d6401ccc7f7b0a836f7c254dcf
JerickTwO <jerickjjtm774@gmail.com>
2025-01-25 12:54:06 -0500

    feat: add JWT authentication and update API views and URLs

--- sistema_tareas/sistema_tareas/api/views.py
+++ sistema_tareas/sistema_tareas/api/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 from tareas.models import Tarea, Comentario, Usuario
 from sistema_tareas.api.serializer import TareaSerializer
 
--- sistema_tareas/sistema_tareas/settings.py
+++ sistema_tareas/sistema_tareas/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
+    "rest_framework_simplejwt",
     "tareas",
 ]
 
@@ -127,3 +128,11 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+
+RESTFRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ]
+}
